14337e01dc1f4bb94e363ec847e825907db0a4a4
Alexander Rush <ar_009@hpc-login2.usc.edu>
2010-11-19 15:24:18 -0800

    ngram cache

---
+++ NGramCache.cpp
@@ -0,0 +1,75 @@
+#include "NGramCache.h"
+
+
+
+// assume we are at a point in the trie, just look one step more
+LogP NgramCache::wordProbFromCache(VocabIndex word, const VocabIndex *context) {
+
+  LogP cur_logp = logp;
+  LogP cur_bow = bow;
+
+  BOtrie *next = trieNode->findTrie(context[i]);
+  if (next) {
+    /*
+     * Accumulate backoff weights 
+     */
+    cur_bow += next->value().bow;
+    //trieNode = next;
+  
+
+    LogP *prob = next->value().probs.find(word);  
+    if (prob) {
+      /*
+       * If a probability is found at this level record it as the 
+       * most specific one found so far and reset the backoff weight.
+       */
+      cur_logp = *prob;
+      cur_bow = LogP_One;
+      //cur_found = i + 1;
+    } 
+  }  
+    
+  return cur_logp + cur_bow;
+}
+
+
+LogP NgramCache::wordProbPrimeCache(VocabIndex word, const VocabIndex *context) {
+  
+  // Reset to original values
+  logp = LogP_Zero;
+  bow = LogP_One;
+  found = 0;
+  
+  trieNode = &contexts;
+  i = 0;
+
+  do {
+    LogP *prob = trieNode->value().probs.find(word);
+    
+    if (prob) {
+      /*
+       * If a probability is found at this level record it as the 
+       * most specific one found so far and reset the backoff weight.
+       */
+      logp = *prob;
+      bow = LogP_One;
+      found = i + 1;
+    } 
+    
+    if  ( context[i] == Vocab_None) break;
+    
+    BOtrie *next = trieNode->findTrie(context[i]);
+    if (next) {
+      /*
+       * Accumulate backoff weights 
+       */
+      bow += next->value().bow;
+      trieNode = next;
+      i ++;
+    } else {
+      break;
+    }
+  } while (1);
+
+  return logp + bow;
+}
