7d4b5110e4b65626b5734d418d294a6dd79a311e
srush@mit.edu <srush@srush-laptop.(none)>
2011-02-07 21:26:04 -0500

    fix lattice first bug

--- hypergraph/ConvertFromFile.cpp
+++ hypergraph/ConvertFromFile.cpp
@@ -68,6 +68,7 @@ int main(int argc, char ** argv) {
       int t2;
       string t3;
       in >> t2 >> t3;
+      cout << "node " <<t2 << " "  << t3 << endl;
       node->set_id(t2);
       node->set_label(t3);
       if (nodes.size()<=node->id()) { 
