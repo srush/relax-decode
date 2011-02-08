3701085aa1b05c5bfe019fc71d5945463b6fa861
srush@mit.edu <srush@srush-laptop.(none)>
2011-02-07 20:09:56 -0500

    Hypergraph Impl

---
+++ hypergraph/HypergraphImpl.cpp
@@ -0,0 +1,160 @@
+#include "HypergraphImpl.h"
+#include "hypergraph.pb.h"
+#include "features.pb.h"
+
+
+namespace Scarab {
+namespace HG {
+
+
+void HypergraphImpl::prune(const HypergraphPrune & prune) {
+  vector <Hypernode *> new_nodes;
+  vector <Hyperedge *> new_edges; 
+
+  int node_count = 0;
+  foreach (Hypernode * tmp_node, nodes()) {
+    HypernodeImpl * node = (HypernodeImpl *)tmp_node;
+    if (prune.nodes.find(node->id()) != prune.nodes.end()) {
+      new_nodes.push_back((Hypernode*)node);
+      node->prune_edges(prune.edges);
+      node->reid(node_count);
+      node_count++;
+    }
+  }
+
+  int edge_count = 0;
+  foreach (Hyperedge * tmp_edge, edges()) {
+    HyperedgeImpl * edge = (HyperedgeImpl *)tmp_edge;
+    if (prune.edges.find(edge->id()) != prune.edges.end()) {
+      new_edges.push_back((Hyperedge*)edge);
+      edge->reid(edge_count);
+      edge_count++;
+    }
+  }
+
+  _nodes = new_nodes;
+  _edges = new_edges;
+  
+} 
+
+
+void HypergraphImpl::write_to_file(const char * file_name) {
+  Hypergraph hgraph;
+
+
+  foreach (HNode my_node, nodes()) {
+    Hypergraph_Node * node = hgraph.add_node();
+    
+    //string feat_str = svector_str<int, double>(my_node->fvector()); 
+    //node->SetExtension(node_fv, feat_str);
+    
+    convert_node(my_node, node);
+    //str_vector * features = svector_from_str<int, double>(feat_str);
+    
+    // 
+    //Hypernode * forest_node = make_node(node, features);
+
+    //assert (forest_node->
+    
+    foreach (HEdge my_edge, my_node->edges()) {
+      Hypergraph_Edge *edge = node->add_edge();
+      str_vector * features;
+      string feat_str = svector_str<int, double>(my_edge->fvector()); 
+      edge->SetExtension(edge_fv, feat_str);
+
+      foreach (HNode sub_node, my_edge->tail_nodes()) {
+        //int id = edge.tail_node_ids(k); 
+        edge->add_tail_node_ids(sub_node->id()); //push_back( _nodes[id]);
+      }             
+      convert_edge(my_edge, edge);
+    }
+  }
+  //assert (_nodes.size() == (uint)hgraph->node_size());
+  hgraph.set_root(hgraph.root());
+ 
+
+  {
+    fstream output(file_name, ios::out | ios::binary);
+    if (!hgraph.SerializeToOstream(&output)) {
+      assert (false);
+    } 
+  }
+}
+
+void HypergraphImpl::build_from_file(const char * file_name) { 
+  hgraph = new ::Hypergraph();     
+  {
+    fstream input(file_name, ios::in | ios::binary);
+    if (!hgraph->ParseFromIstream(&input)) {
+      assert (false);
+    } 
+  }
+
+  set_up(*hgraph);
+
+  assert (hgraph->node_size() > 0);
+  for (int i = 0; i < hgraph->node_size(); i++) {
+    const Hypergraph_Node & node = hgraph->node(i);
+    
+    string feat_str = node.GetExtension(node_fv);
+    str_vector * features = svector_from_str<int, double>(feat_str);
+    
+    // 
+    Hypernode * forest_node = make_node(node, features);
+
+    //assert (forest_node->
+    assert (_nodes.size() == (uint)node.id());
+    _nodes.push_back(forest_node);
+    assert(_nodes[forest_node->id()]->id() == forest_node->id());
+  }
+
+  int edge_id = 0;
+  for (int i = 0; i < hgraph->node_size(); i++) {
+    const Hypergraph_Node& node = hgraph->node(i);
+    assert (node.id()  == i);
+
+    for (int j=0; j < node.edge_size(); j++) {
+      const Hypergraph_Edge& edge = node.edge(j);
+      str_vector * features;
+      if (edge.HasExtension(edge_fv)) { 
+        const string & feat_str = edge.GetExtension(edge_fv);  
+        features = svector_from_str<int, double>(feat_str);
+      } else {
+        features = new svector<int, double>();
+      }
+
+      vector <Scarab::HG::Hypernode *> tail_nodes;
+      for (int k =0; k < edge.tail_node_ids_size(); k++ ){
+        int id = edge.tail_node_ids(k); 
+        tail_nodes.push_back( _nodes[id]);
+      } 
+      
+
+      
+      Scarab::HG::Hyperedge * forest_edge = new Scarab::HG::HyperedgeImpl(edge.label(), 
+                                                                          features, 
+                                                                          edge_id, 
+                                                                          tail_nodes, 
+                                                                          _nodes[node.id()]);
+      
+      make_edge(edge, forest_edge);      
+      for (int k =0; k < edge.tail_node_ids_size(); k++ ){
+        int id = edge.tail_node_ids(k);
+        ((HypernodeImpl*)_nodes[id])->add_in_edge(forest_edge);
+      }
+      ((HypernodeImpl*)_nodes[node.id()])->add_edge(forest_edge);
+
+      edge_id++;
+      //int for_edge_id = forest_edge->id();
+      _edges.push_back(forest_edge);//[for_edge_id] = forest_edge;
+    }
+    //cout << node.id() << " "<<  _nodes[node.id()]->num_edges() << " " << node.edge_size() << " " << _nodes[node.id()]->is_word() << endl;
+    assert (_nodes[node.id()]->num_edges() == (uint)node.edge_size() );
+  }
+  assert (_nodes.size() == (uint)hgraph->node_size());
+  int root_num = hgraph->root();
+  _root = _nodes[hgraph->root()];//_nodes[_nodes.size()-1];
+  delete hgraph;
+}
+}
+}
---
+++ hypergraph/HypergraphImpl.h
@@ -0,0 +1,222 @@
+#ifndef HYPERGRAPHIMPL_H_
+#define HYPERGRAPHIMPL_H_
+
+#include "Hypergraph.h"
+#include "hypergraph.pb.h"
+#include "features.pb.h"
+
+#include "Weights.h"
+#include "../common.h"
+#include <vector>
+#include <set>
+using namespace std;
+
+namespace Scarab {
+namespace HG {
+
+class HypernodeImpl;
+
+class HyperedgeImpl: public Hyperedge {
+public:
+  virtual ~HyperedgeImpl(){}
+  HyperedgeImpl(const string & label, str_vector * features, 
+                int id, vector <Hypernode *> tail_nodes, 
+                Hypernode * head_node):
+  _id(id),
+    _label(label), _tail_nodes(tail_nodes), 
+    _head_node(head_node), _features(features) {
+    
+  }
+  
+  const Hypernode & tail_node(unsigned int i) const {
+    return * (_tail_nodes[i]);
+  }
+
+  uint num_nodes() const{
+    return _tail_nodes.size();
+  }  
+  
+  const wvector & fvector() const {
+    return *_features;
+  }
+
+  const Scarab::HG::Hypernode & head_node() const { 
+    return (*_head_node);
+  }
+
+  const vector <Scarab::HG::Hypernode*> & tail_nodes() const  {
+    return _tail_nodes;
+  }
+
+  uint id() const {
+    return _id;
+  }
+
+
+  string label() const {
+    return _label;
+  }
+
+  void reid(int new_id) {
+    _id = new_id;
+  }
+
+
+
+  const string _label;  
+  vector <Hypernode *> _tail_nodes;
+  Hypernode * _head_node;
+
+private:
+  int _id;
+  str_vector * _features;
+
+};
+
+
+class HypernodeImpl: public Hypernode {
+public:
+  ~HypernodeImpl(){}
+  HypernodeImpl(const string & label, int id, wvector * features) :
+  _id(id),
+  _label(label), _features(features) {}
+  
+  void add_edge(Hyperedge * edge) {
+    _edges.push_back(edge);
+  }
+
+  void add_in_edge(Hyperedge * edge) {
+    _in_edges.push_back(edge);
+  }
+
+  bool is_terminal() const {
+    return _edges.size() == 0;
+  }
+
+
+  void prune_edges(const set<int> & keep_edges ) {
+    vector < Hyperedge *> new_edges; 
+    vector < Hyperedge *> new_in_edges; 
+    foreach (Hyperedge * edge, _edges) {
+      if (keep_edges.find(edge->id()) != keep_edges.end()) 
+        new_edges.push_back(edge);
+    }
+    foreach (Hyperedge * edge, _in_edges) {
+      if (keep_edges.find(edge->id()) != keep_edges.end()) 
+        new_in_edges.push_back(edge); 
+    }
+
+    _edges = new_edges;
+    _in_edges = new_in_edges;    
+  }
+
+  const Hyperedge & edge(uint i ) const {
+    return *_edges[i];
+  }
+  
+  unsigned int num_edges() const{
+    return _edges.size();
+  }
+
+  unsigned int num_in_edges() const{
+    return _in_edges.size();
+  }
+
+  const Hyperedge & in_edge(uint i) const {
+    return *_in_edges[i];
+  }
+
+  uint id() const {
+    return _id;
+  }
+
+  const vector <Hyperedge*> & edges() const {
+    return _edges;
+  }
+
+  const vector <Hyperedge*> & in_edges() const {
+    return _in_edges;
+  } 
+  
+  void reid(int new_id) {
+    _id = new_id;
+  }
+
+private:  
+   int  _id;
+ public:
+  vector < Hyperedge *> _edges; 
+  const string & _label;
+
+
+private:  
+  wvector * _features;
+  
+  vector <Hyperedge *> _in_edges; 
+};
+
+class HypergraphImpl : public HGraph {
+ public:
+  HypergraphImpl(){}
+  ~HypergraphImpl(){}
+  //HypergraphImpl(const char* filename);//const Hypergraph & pb);
+      
+  const Hypernode & root() const {
+    return *_root;
+  }
+
+  unsigned int num_edges() const{
+    return _edges.size();
+  }
+
+  uint num_nodes() const{
+    return _nodes.size();
+  }
+
+  const Hypernode & get_node(unsigned int i) const {
+    //const ForestNode & node =* (ForestNode*) _nodes[i];
+    //assert (node.id() == i);
+    return *_nodes[i];
+  }
+
+
+  const Hyperedge & get_edge(uint i) const {
+    //const Hyperedge & edge =
+    //assert (edge.id() == i);
+    return *_edges[i];
+  }
+  
+  
+  void build_from_file(const char * file_name );
+
+  const vector <Hypernode*> & nodes() const {
+    return _nodes;
+  }
+  const vector <Hyperedge*> & edges() const {
+    return _edges;
+  }
+
+  void prune(const HypergraphPrune & prune );
+
+ protected:
+  ::Hypergraph * hgraph;
+  Hypernode * _root;
+  vector <Hypernode *> _nodes;
+  vector <Hyperedge *> _edges; 
+
+  virtual Hypernode * make_node(const Hypergraph_Node & node,  wvector * features) {
+    //    cout << "bad make node called!" << endl;
+    return new HypernodeImpl(node.label(), node.id(), features); 
+  }
+
+  virtual void make_edge(const Hypergraph_Edge & edge, const Hyperedge * our_edge) {}
+  virtual void convert_edge(const Hyperedge * our_edge, Hypergraph_Edge * edge ) {}
+  virtual void convert_node(const Hypernode * our_node, Hypergraph_Node * node ) {}
+  virtual void set_up(const Hypergraph & hgraph) {}
+  void write_to_file(const char * file_name);
+};
+
+
+}
+}
+#endif
